d0ae98e2bd5618c6791e8c664bec6f72b656b6c1
Agustin <agustin.canteros@gmail.com>
2022-09-06 16:00:47 -0300

    person update

--- Persistence/Models/Person.cs
+++ Persistence/Models/Person.cs
@@ -6,7 +6,7 @@ namespace Common.Api.Graphql.Persistence.Models
     public class Person : EntityBase, IHasName
     {
         public string Name { get; set; } = string.Empty;
-        
-        public ICollection<Address> Adresses { get; set; }
+
+        public ICollection<Address> Adresses { get; set; } = default!;
     }
 }
